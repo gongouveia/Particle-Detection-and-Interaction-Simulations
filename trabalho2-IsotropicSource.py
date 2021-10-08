4a3a814968bafe149fec5797c5b9098f63f8066b
Gonçalo Paiva Gouveia <68733294+gongouveia@users.noreply.github.com>
2021-10-08 10:36:08 +0100

    Update trabalho2-IsotropicSource.py

--- trabalho2-IsotropicSource.py
+++ trabalho2-IsotropicSource.py
@@ -2,75 +2,53 @@
 """
 Created on Wed Sep 29 19:35:19 2021
 
-@author: gongou00@gmail.com
 """
 
 #import libraries
-
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from numpy import random,sqrt, log, sin, cos, pi, array
+from numpy import random,sqrt, sin, cos, pi
 from pylab import show,hist,subplot,figure
-import pandas as pd
-
-numArrows=100000
-random.seed(68  )
-bins =30
+from matplotlib import pyplot
+from mpl_toolkits.mplot3d import Axes3D
 
 
-#set random numbers
-r1 = random.rand(numArrows)*2*pi
-r2 = random.rand(numArrows)*pi
+numPoints=100000
+random.seed(68)
+bins =25
+A=[]  
+
+#Uniform Spherical Distribution Generator
+def generator(numPoints):
+    theta = random.rand(numPoints)*2*pi
+    z = random.rand(numPoints)*2-1
     
-#coordenadas esfericas
-x= cos(r1)*sin(r2)
-y= sin(r1)*sin(r2)
-z= cos(r2)
-size= sqrt(x**2+y**2+z**2)
+    x = sqrt(1-z**2)*cos(theta)
+    y = sqrt(1-z**2)*sin(theta)
+    return[x,y,z]
 
 
-# set plots
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.quiver([0],[0],[0],[x],[y],[z],linewidths=0.1,edgecolors= "k",length=2)
 
-
-ax.set_xlim3d([-2.0, 2.0])
-ax.set_xlabel('X')
-
-ax.set_ylim3d([-2.0, 2.0])
-ax.set_ylabel('Y')
-
-ax.set_zlim3d([-2, 2])
-ax.set_zlabel('Z')
-
-ax.set_title('isotropic source') 
-plt.show()
-
-
+A = generator(numPoints)
+    
+#Drawing histogram
 figure(figsize=(10,10))
 ax4=subplot(211)    
-hist(x,bins,histtype="step")  
-hist(y,bins,histtype="step")     
-hist(z,bins,histtype="step")     
+hist(A[0],bins,histtype="step")  
+hist(A[1],bins,histtype="step")        
+hist(A[2],bins,histtype="step")     
 ax4.legend(["x","y","z"], frameon=True, loc=1)
-
 ax4.set_title("r3 dataset - seed 64 ")
 show()
 
 
-# organise dataset table
-A=[]
-for i in range(numArrows):
-    A.append([round(x[i],3),round(y[i],3),round(z[i],3), round(size[i],3)])
+#Drawing spherical distribution
+fig = pyplot.figure()
+ax = Axes3D(fig)
+ax.scatter(A[0],A[1],A[2],s=1, color="k")
+ax.legend(["Uniform Datapoints"], frameon=True, loc=1)
+ax.set_title('isotropic source') 
+show()
 
-numpy_array = array(A)
 
-df = pd.DataFrame(numpy_array, columns=["x","y","z","length"])
-df.loc[:4].style.set_properties(**{'background-color': 'black',
-                           'color': 'lawngreen',
-                           'border-color': 'white'})
 
-#prient first 40 dataset points
-print(df.head(40))
+
 
